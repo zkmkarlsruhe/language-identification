5ae0ee262563e4eec1891d17e98e387db59a75d8
bethge <bethge@zkm.de>
2021-03-09 14:01:27 +0100

    more config

--- main.py
+++ main.py
@@ -1,27 +1,14 @@
-input_dir = config["common_voice_dir"]
-output_dir = config["chopped_wavs"]
-max_chops = config["max_chops"]
 
 
 command = "python mozilla_to_wav.py --input_dir " + input_dir \
             "--output_dir" + output_dir \
             "--total_chops" + max_chops
 
-source = output_dir + 
-img_dir = config[""]
-feature_type = config[""]
-feature_nu = config[""]
-augment_nu = config[""]
-
 command = "process_wav_files.py --source " + \
     "--img_dir" + \
     "--run_as_thread" + \
     "--feature_type" + \
     "--feature_nu" + \
-if augment:
-    command += "--augment" 
-    command += "--augment_nu" + augment_nu
-
 
 os.system(command)
 
