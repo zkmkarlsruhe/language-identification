45ac0f792450e9a4c9ea7048d3bb1d1cd317304a
Paul Bethge <paul.bethge@zkm.de>
2021-05-26 21:46:46 +0200

    new models

---
+++ src/models/ResNet.py
@@ -0,0 +1,31 @@
+from tensorflow.keras.layers import GlobalAveragePooling2D, Dropout, LayerNormalization
+from tensorflow.keras.layers import Dense, Permute, Input
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.applications import ResNet50
+
+from src.utils.training_utils import get_feature_layer
+
+
+def create_model(config):
+
+	audio_length_s = config["audio_length_s"] 
+	sample_rate = config["sample_rate"]
+	feature_nu = config["feature_nu"]
+	feature_type = config["feature_type"]
+	input_length = audio_length_s * sample_rate
+
+	inputs = Input((input_length, 1), name='input')
+	feature_extractor = get_feature_layer(feature_type, feature_nu, sample_rate)
+	res_net = ResNet50(include_top=False, weights=None, input_shape=(None,None,1))
+
+	model = Sequential()
+	model.add(inputs)
+	model.add(feature_extractor)
+	model.add(LayerNormalization())
+	model.add(Permute((2, 1, 3)))
+	model.add(res_net)
+	model.add(GlobalAveragePooling2D())
+	model.add(Dropout(0.5))
+	model.add(Dense(len(config["languages"]), activation='softmax'))
+
+	return model
--- src/models/attRnn.py
+++ src/models/attRnn.py
@@ -1,49 +1,58 @@
-from tensorflow.keras.layers import GlobalAveragePooling2D, Dropout, BatchNormalization
+from tensorflow.keras.layers import Permute, Dropout, BatchNormalization
 from tensorflow.keras.layers import Input, Dense, LSTM, Conv2D, Bidirectional
-from tensorflow.keras.layers import Permute, Lambda, Dot, Softmax
+from tensorflow.keras.layers import LayerNormalization, Lambda, Dot, Softmax
 from tensorflow.keras.models import Model
-from tensorflow.keras.applications import EfficientNetB7
 
 import tensorflow.keras.backend as K
 
-NAME = "ATTENTIONRNN"
+from src.utils.training_utils import get_feature_layer
 
 
 def create_model(config):
 
-    input_shape = config["input_shape"]
-    inputs = Input(shape=input_shape, name='input')
+	audio_length_s = config["audio_length_s"] 
+	sample_rate = config["sample_rate"]
+	feature_nu = config["feature_nu"]
+	feature_type = config["feature_type"]
+	input_length = audio_length_s * sample_rate
 
-    x = Permute((2, 1, 3))(inputs)
+	feature_extractor = get_feature_layer(feature_type, feature_nu, sample_rate)
 
-    x = Conv2D(10, (5, 1), activation='relu', padding='same')(x)
-    x = BatchNormalization()(x)
-    x = Conv2D(1, (5, 1), activation='relu', padding='same')(x)
-    x = BatchNormalization()(x)
+	inputs = Input((input_length, 1), name='input')
 
-    # keras.backend.squeeze(x, axis)
-    x = Lambda(lambda q: K.squeeze(q, -1), name='squeeze_last_dim')(x)
+	x = feature_extractor(inputs)
+	x = LayerNormalization()(x)
 
-    x = Bidirectional(LSTM(64, return_sequences=True)
-                        )(x)  # [b_s, seq_len, vec_dim]
-    x = Bidirectional(LSTM(64, return_sequences=True)
-                        )(x)  # [b_s, seq_len, vec_dim]
+	x = Permute((2, 1, 3))(x)
 
-    xFirst = Lambda(lambda q: q[:, -1])(x)  # [b_s, vec_dim]
-    query = Dense(128)(xFirst)
+	x = Conv2D(10, (5, 1), activation='relu', padding='same')(x)
+	x = BatchNormalization()(x)
+	x = Conv2D(1, (5, 1), activation='relu', padding='same')(x)
+	x = BatchNormalization()(x)
 
-    # dot product attention
-    attScores = Dot(axes=[1, 2])([query, x])
-    attScores = Softmax(name='attSoftmax')(attScores)  # [b_s, seq_len]
+	x = Lambda(lambda q: K.squeeze(q, -1), name='squeeze_last_dim')(x)
 
-    # rescale sequence
-    attVector = Dot(axes=[1, 1])([attScores, x])  # [b_s, vec_dim]
+	x = Bidirectional(LSTM(128, return_sequences=True))(x)  # [b_s, seq_len, vec_dim]
+	x = Bidirectional(LSTM(128, return_sequences=True))(x)  # [b_s, seq_len, vec_dim]
 
-    x = Dense(64, activation='relu')(attVector)
-    x = Dense(32)(x)
+	xFirst = Lambda(lambda q: q[:, -1])(x)  # [b_s, vec_dim]
+	query = Dense(256)(xFirst)
 
-    output = Dense(len(config["languages"]), activation='softmax', name='output')(x)
+	# dot product attention
+	attScores = Dot(axes=[1, 2])([query, x])
+	attScores = Softmax(name='attSoftmax')(attScores)  # [b_s, seq_len]
 
-    model = Model(inputs=[inputs], outputs=[output])
+	# rescale sequence
+	attVector = Dot(axes=[1, 1])([attScores, x])  # [b_s, vec_dim]
 
-    return model
+	x = Dense(256, activation='relu')(attVector)	
+	x = Dropout(0.25)(x)
+	x = Dense(64)(x)
+	x = Dropout(0.25)(x)
+
+
+	output = Dense(len(config["languages"]), activation='softmax', name='output')(x)
+
+	model = Model(inputs=[inputs], outputs=[output])
+
+	return model
